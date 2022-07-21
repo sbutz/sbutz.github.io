3ed25b5e88d9340f8735ddede02bb8719493e338
Stefan Butz <stefan260699@gmail.com>
2022-07-21 12:28:03 +0200

    typewriter effect on subtitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { createTheme, ThemeProvider, Stack, Typography } from '@mui/material';
 
+import TypeWriter from 'typewriter-effect';
+
 const theme = createTheme({
   typography: {
     fontFamily: "'JetBrains Mono', monospace",
   },
 });
+theme.typography.h1 = {
+  fontFamily: theme.typography.fontFamily,
+  fontSize: '3rem',
+  [theme.breakpoints.up('md')]: {
+    fontSize: '8rem',
+  },
+};
+theme.typography.h3 = {
+  fontFamily: theme.typography.fontFamily,
+  fontSize: '1.2rem',
+  [theme.breakpoints.up('md')]: {
+    fontSize: '2.4rem',
+  },
+};
 
 function App() {
   return (
@@ -12,6 +28,19 @@ function App() {
       <Stack height="100vh" justifyContent="center">
         <Typography variant='h1' textAlign="center">
           Stefan Butz
+        </Typography>
+        <Typography variant="h3" textAlign="center">
+          <TypeWriter
+            onInit={(typewriter) => {
+              typewriter.typeString('Freelancer')
+                .pauseFor(500)
+                .typeString(' & <strong><span style="color: #27ae60;">Software Developer</span></strong>')
+                .pauseFor(500)
+                .deleteAll()
+                .typeString('Ready for <strong><span style="color: #27ae60;">Your</span></strong> Project!')
+                .start();
+            }}
+          />
         </Typography>
       </Stack>
     </ThemeProvider>

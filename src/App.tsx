6a57079d3ed7d34bbac013055160d97be5d85bcf
Stefan Butz <stefan260699@gmail.com>
2022-07-21 17:45:28 +0200

    first shot at a homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { createTheme, ThemeProvider, Stack, Typography } from '@mui/material';
-
+import { createTheme, ThemeProvider, AppBar, Toolbar, IconButton, Stack, Link,
+  Container, Typography, } from '@mui/material';
+import { GitHub, LinkedIn, Mail } from '@mui/icons-material';
 import TypeWriter from 'typewriter-effect';
 
 const theme = createTheme({
@@ -31,6 +32,42 @@ theme.typography.h3 = {
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <AppBar position="fixed" color="transparent" elevation={1}>
+      <Container maxWidth="md">
+        <Toolbar disableGutters={true}>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1, pl: 1 }}>
+            SB
+          </Typography>
+          <Link
+            target="_blank"
+            rel="noopener"
+            href="mailto:mail@butz.st"
+            component={IconButton}
+            color="inherit"
+          > 
+            <Mail sx={{ fontSize: "1.5rem" }} />
+          </Link>
+          <Link
+            target="_blank"
+            rel="noopener"
+            href="https://linkedin.com/in/stefan-butz"
+            component={IconButton}
+            color="inherit"
+          > 
+            <LinkedIn sx={{ fontSize: "1.5rem" }} />
+          </Link>
+          <Link
+            target="_blank"
+            rel="noopener"
+            href="https://github.com/sbutz"
+            component={IconButton}
+            color="inherit"
+          > 
+            <GitHub sx={{ fontSize: "1.5rem" }} />
+          </Link>
+        </Toolbar>
+      </Container>
+      </AppBar>
       <Stack height="100vh" justifyContent="center">
         <Typography variant='h1' textAlign="center">
           Stefan Butz
@@ -39,9 +76,9 @@ function App() {
           <TypeWriter
             onInit={(typewriter) => {
               typewriter.typeString('Freelancer')
-                .pauseFor(500)
                 .typeString(` & <strong><span style="color: ${theme.palette.primary.main} ;">Software Developer</span></strong>`)
-                .pauseFor(500)
+                .pauseFor(1000)
+                .changeDeleteSpeed(20)
                 .deleteAll()
                 .typeString(`Ready for <strong><span style="color: ${theme.palette.primary.main};">Your</span></strong> Project!`)
                 .start();

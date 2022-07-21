3af4befebc2905652397c50fae8cc705d5589f40
Stefan Butz <stefan260699@gmail.com>
2022-07-21 11:27:47 +0200

    add mui with jetbrains font

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { createTheme, ThemeProvider, Stack, Typography } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'JetBrains Mono', monospace",
+  },
+});
+
 function App() {
   return (
-    <p>This page is under construction.</p>
+    <ThemeProvider theme={theme}>
+      <Stack height="100vh" justifyContent="center">
+        <Typography variant='h1' textAlign="center">
+          Stefan Butz
+        </Typography>
+      </Stack>
+    </ThemeProvider>
   );
 }
 
